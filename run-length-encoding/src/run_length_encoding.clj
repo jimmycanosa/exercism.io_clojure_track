0754ea1adb93f13917de37276cba7a742d1865dc
Jimmy <jimmycanosajr@gmail.com>
2021-05-20 23:00:25 +0800

    Run length encoding solution

--- run-length-encoding/src/run_length_encoding.clj
+++ run-length-encoding/src/run_length_encoding.clj
@@ -2,8 +2,44 @@
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (->> plain-text
+       (partition-by identity)
+       (map #(if (= (count %) 1)
+               %
+               [(count %) (first %)]))
+       flatten
+       (apply str)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [cipher-text])
+  [cipher-text]
+  (->> cipher-text
+       (re-seq #"\d*\s|\d*\w")
+       (map #(if (= (count %) 1)
+               %
+               (repeat (apply read-string (re-seq #"\d+" %)) (last %))))
+       flatten
+       (apply str)))
+
+(comment
+
+  (defn run-length-encode
+    [s]
+    (->> s
+         (partition-by identity)
+         (mapcat #(if (= 1 (count %))
+                    [(first %)]
+                    [(count %) (first %)]))
+         (apply str)))
+
+  (defn run-length-decode
+    [s]
+    (->> s
+         (re-seq #"[1-9]*[a-zA-Z ]")
+         (mapcat #(if (= 1 (count %))
+                    %
+                    (repeat (->> % drop-last (apply str) Integer.) (last %))))
+         (apply str)))
+
+  nil)
