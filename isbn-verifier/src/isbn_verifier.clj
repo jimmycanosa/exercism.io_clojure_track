98e25ed6f37214762250dd2b0b88ac086b46ac88
Jimmy <jimmycanosajr@gmail.com>
2021-05-21 22:29:51 +0800

    ISBN verifier 2nd Solution (better)

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,10 +1,13 @@
 (ns isbn-verifier)
 
 (defn isbn? [isbn]
-  (if (= (count (re-seq #"\d|[X]" isbn)) (count (re-seq #"\d|[X]$" isbn)) 10)
-    (let [isbn (into [] (re-seq #"\d|[X]$" isbn))]
-      (if (zero? (mod (->> (if (= "X" (last isbn)) (assoc isbn 9 "10") isbn)
-                           (map read-string)
-                           (map * (range 10 0 -1))
-                           (reduce +)) 11)) true false))
-    false))
+  (let [isbn (->> (clojure.string/replace isbn "-" "")
+                  (re-matches #"^\d{9}[\dX]$"))]
+    (if (= 10 (count isbn))
+      (->> isbn
+           (map #(if (= \X %) 10 (Character/digit % 10)))
+           (map * (range 10 0 -1))
+           (reduce +)
+           (#(mod % 11))
+           zero?)
+      false)))
