caebc92e4e308419155133ed7105793cf0da4b98
Jimmy <jimmycanosajr@gmail.com>
2021-05-21 22:27:55 +0800

    ISBN verifier solution

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,10 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isbn? [isbn]
+  (if (= (count (re-seq #"\d|[X]" isbn)) (count (re-seq #"\d|[X]$" isbn)) 10)
+    (let [isbn (into [] (re-seq #"\d|[X]$" isbn))]
+      (if (zero? (mod (->> (if (= "X" (last isbn)) (assoc isbn 9 "10") isbn)
+                           (map read-string)
+                           (map * (range 10 0 -1))
+                           (reduce +)) 11)) true false))
+    false))
