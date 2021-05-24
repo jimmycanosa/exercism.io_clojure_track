e2f20de04bf8869db21e7018a625d768ca39b584
Jimmy <jimmycanosajr@gmail.com>
2021-05-24 13:19:54 +0800

    Scrabble score solution

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,9 +1,16 @@
 (ns scrabble-score)
 
-(defn score-letter [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn score-letter [c]
+  (condp contains? (clojure.string/upper-case c)
+    #{"A" "E" "I" "O" "U" "L" "N" "R" "S" "T"} 1
+    #{"D" "G"}                                 2
+    #{"B" "C" "M" "P"}                         3
+    #{"F" "H" "V" "W" "Y"}                     4
+    #{"K"}                                     5
+    #{"J" "X"}                                 8
+    #{"Q" "Z"}                                10))
 
-(defn score-word [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn score-word [word]
+  (->> word
+       (map score-letter)
+       (reduce +)))
